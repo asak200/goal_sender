68f9af97f90093355688a999a0e085107772252e
asak200 <abdoorahmansk2004@gmail.com>
2024-07-27 18:16:46 +0300

    l

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ package_name = 'goal_sender'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
