c956fc9206cf21ca7c353c3b44769dba1a5e5111
asak200 <abdoorahmansk2004@gmail.com>
2024-08-30 23:21:01 +0300

    hk

--- qr_reader/qr_cam.py
+++ qr_reader/qr_cam.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 import cv2
-# from pyzbar.pyzbar import decode
+from pyzbar.pyzbar import decode
 from std_msgs.msg import String
 
 class QRCodeScannerNode(Node):
@@ -31,7 +31,7 @@ class QRCodeScannerNode(Node):
             return
         
         # Decode QR codes from the frame
-        # qr_codes = decode(frame)
+        qr_codes = decode(frame)
         
         # for qr_code in qr_codes:
         #     qr_data = qr_code.data.decode('utf-8')
@@ -48,23 +48,23 @@ class QRCodeScannerNode(Node):
         
         # # Display the frame with QR codes (optional, for debugging)
         # frame_with_qr = self.draw_qr_codes(frame, qr_codes)
-        cv2.imshow('QR Code Scanner', frame)
-        cv2.waitKey(1)  # Needed to keep the window open
+        # cv2.imshow('QR Code Scanner', frame)
+        # cv2.waitKey(1)  # Needed to keep the window open
         
         
-        # if len(qr_codes) == 0:
-        #     return
-        # qr_data = qr_codes[0].data.decode('utf-8')
-        # if self.prev_qr == qr_data:
-        #     return
-        # self.prev_qr = qr_data
-        # # self.get_logger().info(f"QR Code detected: {self.prev_qr}")
-        # self.get_logger().info(f"QR Code noooooow: {qr_data}")
+        if len(qr_codes) == 0:
+            return
+        qr_data = qr_codes[0].data.decode('utf-8')
+        if self.prev_qr == qr_data:
+            return
+        self.prev_qr = qr_data
+        # self.get_logger().info(f"QR Code detected: {self.prev_qr}")
+        self.get_logger().info(f"QR Code noooooow: {qr_data}")
         
-        # # Publish the QR code data
-        # msg = String()
-        # msg.data = qr_data
-        # self.qr_order_pub.publish(msg)
+        # Publish the QR code data
+        msg = String()
+        msg.data = qr_data
+        self.qr_order_pub.publish(msg)
     
     def draw_qr_codes(self, frame, qr_codes):
         for qr_code in qr_codes:
